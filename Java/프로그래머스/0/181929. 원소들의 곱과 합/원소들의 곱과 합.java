3742a8a58dbd38cf9727b16c1bdb7a9451ab7133
jiyoeo <jiyoeo2@gmail.com>
2026-09-18 23:50:22 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int multiple = 1;
+        for (int i = 0; i < num_list.length; i++){
+            sum += num_list[i];
+            multiple *= num_list[i];
+        }
+        if (multiple < sum*sum) answer = 1;
+        return answer;
+    }
+}
